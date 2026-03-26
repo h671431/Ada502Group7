6d34d2a5d8a98d9944d394597527dbf40a7fc3bc
mathias <669851@stud.hvl.no>
2026-03-26 15:16:09 +0100

    Deployment ready: Docker container running on NREC server

--- src/frcm/web.py
+++ src/frcm/web.py
@@ -77,12 +77,10 @@ def landing_page():
             <div>
                 <label>Breddegrad (lat):</label>
                 <input type="number" id="latitude" placeholder="f.eks. 60.3" step="0.001" readonly />
-                
+            </div>
+            <div>    
                 <label>Lengdegrad (lon):</label>
                 <input type="number" id="longitude" placeholder="f.eks. 5.3" step="0.001" readonly />
-                
-                <label>Høyde (m):</label>
-                <input type="number" id="altitude" placeholder="Valgfritt" step="1" />
                 
                 <button onclick="fetchWeather()">Hent værdata</button>
             </div>
